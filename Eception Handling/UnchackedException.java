8d871e7785145aa728b649398c31a20cf36f4e00
Biswajit6997 <biswajitde1996@gmail.com>
2024-01-26 21:01:15 +0530

    add try and catch and throws and throw unchecked exception

---
+++ Eception Handling/UnchackedException.java
@@ -0,0 +1,19 @@
+// Hireachy of  Exception class and checked unchacked Exception 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UnchackedException {
+    public static void main(String[] args) {
+        int result=7/0;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       try {
+        String str= br.readLine();
+    } catch (IOException e) {
+        
+        e.printStackTrace();
+    }
+    }
+    
+}
