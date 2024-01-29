d1e92d5caa17d0ea42cc272d8a7e3238d3f2334f
Biswajit6997 <biswajitde1996@gmail.com>
2024-01-29 22:29:50 +0530

    add string

---
+++ String/Eg1.java
@@ -0,0 +1,12 @@
+public class Eg1 {
+
+    public static void main(String[] args) {
+        String a="biswajit";
+        String b="biswajit";
+        String ab=new String("biswajit");
+        System.out.println(a==b);// True
+        System.out.println(a==ab);//False
+
+
+    }
+}
