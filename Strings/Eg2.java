8d5e86521c43da0f88049d38cf03d9c1df9923f6
Biswajit6997 <biswajitde1996@gmail.com>
2024-01-30 20:15:11 +0530

    add stringd

---
+++ Strings/Eg2.java
@@ -0,0 +1,11 @@
+public class Eg2 {
+    public static void main(String[] args) {
+        String s1="pwskills";
+        String s2="pwskills";
+        String s3=new String("pwskills");
+        String s4=new String("PWSKILLS");
+        System.out.println(s1==s2);
+        System.out.println(s2.equals(s3));//true
+        System.out.println(s3.equals(s4));//false
+    }
+}
