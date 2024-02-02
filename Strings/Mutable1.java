731817da5c4233787b34ef1bdb7a19a09e8408bd
Biswajit6997 <biswajitde1996@gmail.com>
2024-02-02 13:03:41 +0530

    add pangram programme  string

---
+++ Strings/Mutable1.java
@@ -0,0 +1,17 @@
+
+// Mutable Strings
+public class Mutable1 {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Biswajit");//Mutable String
+        sb.append("Dey");
+        
+        System.out.println(sb);
+        StringBuffer sb1=new StringBuffer("Biswajit");//Mutable String
+        sb1.append("Dey");
+        
+        System.out.println(sb1);
+
+
+    }
+    
+}
