2ca622019ab2f6defe6186b66048576f345b7342
Biswajit6997 <biswajitde1996@gmail.com>
2024-02-03 22:44:52 +0530

    add ArrayList and linkedList and ArrayDequue

--- ArrayList/PriorityQueueEg.java
+++ ArrayList/PriorityQueueEg.java
@@ -1,3 +1,21 @@
+
+// PriorityQueue follow Min Heap data Structure
+
+import java.util.PriorityQueue;
+
 public class PriorityQueueEg {
-    
+    public static void main(String[] args) {
+        PriorityQueue pq=new PriorityQueue<>();
+        pq.add(25);
+        
+        pq.add(10);
+        pq.add(100);
+        pq.add(45);
+        pq.add(35);
+        pq.add(123);
+        pq.add(435);
+        pq.add(25);
+        // pq.add("pw");
+        System.out.println(pq);//[10, 25, 100, 45, 35, 123, 435]
+    }
 }
