a0a3f7f683181b618f3d213012a78ee9deec2a0d
Biswajit6997 <biswajitde1996@gmail.com>
2024-02-17 23:25:17 +0530

    add line sepetor

---
+++ Prac/rev.java
@@ -0,0 +1,19 @@
+public class rev {
+    
+
+// Java program to print a new line in string 
+// Using System.lineSeparator() method 
+
+// Main class 
+ 
+	// Main Driver Code 
+	public static void main(String[] args) 
+	{ 
+		// Calling the System.lineSeparator() function to 
+		// print newline in between some specified strings 
+		String newline = System.lineSeparator(); 
+
+		// Printing new line 
+		System.out.println("GFG" + newline + "gfg"); 
+	} 
+}
