a3bc2a2ae62510fea038432ce10cc87c4e362d14
Biswajit6997 <biswajitde1996@gmail.com>
2024-02-19 23:04:58 +0530

    add file

---
+++ Prac/abc.java
@@ -0,0 +1,16 @@
+public class abc {
+    
+    public static void main(String[] args) {
+        ArrayList<string> al = new ArrayList<string>();
+        al.add("this");
+        al.add("is");
+        al.add("lots");
+        al.add("of");
+        al.add("fun");
+        al.add("for");
+        al.add("every");
+        al.add("Java");
+        al.add("programmer");
+}
+}
+   
