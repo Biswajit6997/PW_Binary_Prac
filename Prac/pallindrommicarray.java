7382736299a15ccbe6179cf35276fa34da5401ff
Biswajit6997 <biswajitde1996@gmail.com>
2024-03-01 20:25:37 +0530

    add prac

---
+++ Prac/pallindrommicarray.java
@@ -0,0 +1,21 @@
+public class pallindrommicarray {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,2,1,4};
+        int flag=0;
+        int n=arr.length;
+        for(int i=0;i<n/2;i++){
+            if (arr[i]!=arr[n-i-1]) {
+                System.out.println("Not a pallindrmic array");
+                flag=1;
+               break;
+            }
+
+            
+        }
+        if(flag==0){
+            System.out.println("Pallindromic array");
+
+        }
+        
+    }
+}
