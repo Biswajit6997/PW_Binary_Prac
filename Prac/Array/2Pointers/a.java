03d68d2a1e0a627558273b4546a3a46a083edcaf
Biswajit6997 <biswajitde1996@gmail.com>
2024-04-27 23:16:02 +0530

    add files

---
+++ Prac/Array/2Pointers/a.java
@@ -0,0 +1,19 @@
+package 2Pointers;
+
+public class a {
+    public static void main(String[] args) {
+        
+    public static void main(String[] args) {
+        ArrayList<string> al = new ArrayList<string>();
+        al.add("this");
+        al.add("is");
+        al.add("lots");
+        al.add("of");
+        al.add("fun");
+        al.add("for");
+        al.add("every");
+        al.add("Java");
+        al.add("programmer");
+}
+    }
+}
